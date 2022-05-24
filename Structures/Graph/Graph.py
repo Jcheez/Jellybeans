4dfc90f5fd91027078c41fb61be4f6d3715703d0
Jcheez <jiajunlee2@gmail.com>
2022-05-24 22:44:33 +0800

    Graph Enhancements completed with UTs (isDag, isTree, isUnweighted)

--- Structures/Graph/Graph.py
+++ Structures/Graph/Graph.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
+from Jellybeans.Structures import Queue
 from Jellybeans.Exceptions.GraphProperty import _GraphProperty
+from .pfuncs import _DFS_tree
 
 class Graph:
 
     def __init__(self) -> None:
         self.__adjList = {}
         self.__reachableBy = {}
+        self.__unweighted = True
+        self.__first_weight = None
 
     def add_vertex(self, nodeId:int) -> None:
         if nodeId not in self.__adjList and nodeId not in self.__reachableBy:
@@ -33,7 +37,7 @@ class Graph:
         return list(self.__adjList.keys())
 
     def num_vertices(self) -> int:
-        return len(self.list_vertices())
+        return len(self.__adjList)
 
     def add_edge(self, vFrom:int, vTo:int, weight:int = 1) -> None:
         if vFrom == vTo:
@@ -45,6 +49,10 @@ class Graph:
             if v == vTo:
                 raise _GraphProperty(f"Edge {vFrom} -> {vTo} already present in graph")
         edges.append((vTo, weight))
+        if self.__first_weight is None:
+            self.__first_weight = weight
+        else:
+            self.__unweighted = False if weight != self.__first_weight else True
         self.__reachableBy[vTo].append(vFrom)
     
     def add_bidirected_edge(self, vFrom:int, vTo:int, weight:tuple):
@@ -120,3 +128,76 @@ class Graph:
             for vTo,weight in lst:
                 res[mapping[vFrom]][mapping[vTo]] = weight
         return mapping, res
+
+    def is_tree(self) -> bool:
+        visited = []
+        parent = []
+        mapping = {}
+        counter = 0
+        vertices = self.list_vertices()
+        for v in vertices:
+            visited.append(0)
+            parent.append(-1)
+            mapping[v] = counter
+            counter += 1
+        parent[0] = 0
+        if not _DFS_tree(visited, vertices[0], self.__adjList, mapping, parent):
+            return False
+        for visit in visited:
+            if visit == 0:
+                return False
+        return True
+
+    def is_unweighted(self) -> bool:
+        return self.__unweighted
+
+    def is_DAG(self) -> bool:
+       
+        def topo_sort(graph:Graph) -> list:
+            '''
+            Performs a topological sort on the graph. Based on Kahn's Algorithm \n
+            Args:
+                graph: Graph object
+            Returns:
+                List containing a valid topological ordering
+            '''
+            in_degree = []
+            mapping = {}
+            toposort = []
+            vertices = graph.list_vertices()
+            inv_map = [0 for _ in vertices]
+            adj_list = graph.to_adjList()
+            counter = 0
+            q = Queue()
+            for v in vertices:
+                in_degree.append(0)
+                mapping[v] = counter
+                inv_map[counter] = v
+                counter += 1
+            for _, vTo,_ in graph.to_edgeList():
+                in_degree[mapping[vTo]] += 1
+
+            for idx in range(len(in_degree)):
+                if in_degree[idx] == 0:
+                    q.enqueue(inv_map[idx])
+
+            while not q.isEmpty():
+                tex = q.dequeue()
+                toposort.append(tex)
+                for v,_ in adj_list[tex]:
+                    in_degree[mapping[v]] -= 1
+                    if in_degree[mapping[v]] == 0:
+                        q.enqueue(v)
+            return toposort
+
+        toposort = topo_sort(self)
+        if len(toposort) < len(self.__adjList):
+            return False
+        idxs = {toposort[idx]: idx for idx in range(len(toposort))}
+        for v,edges in self.__adjList.items():
+            for edge,_ in edges:
+                idx_v = idxs[v]
+                idx_w = idxs[edge]
+                if idx_v > idx_w:
+                    return False
+        return True
---
+++ Structures/Graph/pfuncs.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+def _DFS_tree(visited: list, source: int, adj_list:dict, mapping:dict, parent_lst:list) -> bool:
+    '''
+    This is a modified version of Depth First Search used to check if a graph is a tree \n
+    Args:
+        visited: List to show if a vertex has been visited
+        source: Source Vertex
+        adj_List: Graph structure stored in the form of an adjacency list
+        mapping: Mapping of vertice number to index number
+        parent_lst: List of parents of a node
+    '''
+    visited[mapping[source]] = 1
+    for neighbor, _ in adj_list[source]:
+        if visited[mapping[neighbor]] == 0:
+            parent_lst[mapping[neighbor]] = mapping[source]
+            if not _DFS_tree(visited, neighbor, adj_list, mapping, parent_lst):
+                return False
+        else:
+            if parent_lst[mapping[source]] != mapping[neighbor]:
+                return False
+    return True
